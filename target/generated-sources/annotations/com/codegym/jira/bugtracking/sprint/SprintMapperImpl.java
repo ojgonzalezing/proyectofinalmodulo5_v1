833aa754bb344f2e42c59da066d8d9c95f2251d8
ojgonzalezing <ojgonzalezing@outlook.com>
2025-09-05 17:05:48 -0500

    Configuracion probada contenedores Docker para BDs

---
+++ target/generated-sources/annotations/com/codegym/jira/bugtracking/sprint/SprintMapperImpl.java
@@ -0,0 +1,99 @@
+package com.codegym.jira.bugtracking.sprint;
+
+import com.codegym.jira.bugtracking.sprint.to.SprintTo;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-09-05T15:59:48-0500",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.16 (Microsoft)"
+)
+@Component
+public class SprintMapperImpl implements SprintMapper {
+
+    @Override
+    public Sprint toEntity(SprintTo to) {
+        if ( to == null ) {
+            return null;
+        }
+
+        Sprint sprint = new Sprint();
+
+        sprint.setCode( to.getCode() );
+        sprint.setStatusCode( to.getStatusCode() );
+        if ( to.getProjectId() != null ) {
+            sprint.setProjectId( to.getProjectId() );
+        }
+
+        return sprint;
+    }
+
+    @Override
+    public List<Sprint> toEntityList(Collection<SprintTo> tos) {
+        if ( tos == null ) {
+            return null;
+        }
+
+        List<Sprint> list = new ArrayList<Sprint>( tos.size() );
+        for ( SprintTo sprintTo : tos ) {
+            list.add( toEntity( sprintTo ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public SprintTo toTo(Sprint entity) {
+        if ( entity == null ) {
+            return null;
+        }
+
+        Long id = null;
+        String code = null;
+        String statusCode = null;
+        Long projectId = null;
+
+        id = entity.getId();
+        code = entity.getCode();
+        statusCode = entity.getStatusCode();
+        projectId = entity.getProjectId();
+
+        SprintTo sprintTo = new SprintTo( id, code, statusCode, projectId );
+
+        sprintTo.setEnabled( entity.isEnabled() );
+
+        return sprintTo;
+    }
+
+    @Override
+    public List<SprintTo> toToList(Collection<Sprint> entities) {
+        if ( entities == null ) {
+            return null;
+        }
+
+        List<SprintTo> list = new ArrayList<SprintTo>( entities.size() );
+        for ( Sprint sprint : entities ) {
+            list.add( toTo( sprint ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public Sprint updateFromTo(SprintTo sprintTo, Sprint sprint) {
+        if ( sprintTo == null ) {
+            return sprint;
+        }
+
+        sprint.setCode( sprintTo.getCode() );
+        sprint.setStatusCode( sprintTo.getStatusCode() );
+
+        sprint.setProjectId( SprintMapper.checkProjectBelong(sprintTo.getProjectId(), sprint) );
+
+        return sprint;
+    }
+}
---
+++ target/generated-sources/annotations/com/codegym/jira/bugtracking/tree/NodeMapperImpl.java
@@ -0,0 +1,91 @@
+package com.codegym.jira.bugtracking.tree;
+
+import com.codegym.jira.bugtracking.ObjectType;
+import com.codegym.jira.bugtracking.project.to.ProjectTo;
+import com.codegym.jira.bugtracking.sprint.to.SprintTo;
+import com.codegym.jira.bugtracking.task.to.TaskTo;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-09-05T15:59:49-0500",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.16 (Microsoft)"
+)
+@Component
+public class NodeMapperImpl implements NodeMapper {
+
+    @Override
+    public NodeTo fromProject(ProjectTo project) {
+        if ( project == null ) {
+            return null;
+        }
+
+        long id = 0L;
+        String code = null;
+        Long parentId = null;
+
+        if ( project.getId() != null ) {
+            id = project.getId();
+        }
+        code = project.getCode();
+        parentId = project.getParentId();
+
+        ObjectType type = ObjectType.PROJECT;
+
+        NodeTo nodeTo = new NodeTo( id, code, type, parentId );
+
+        nodeTo.setEnabled( project.isEnabled() );
+
+        return nodeTo;
+    }
+
+    @Override
+    public NodeTo fromSprint(SprintTo sprint) {
+        if ( sprint == null ) {
+            return null;
+        }
+
+        long id = 0L;
+        String code = null;
+
+        if ( sprint.getId() != null ) {
+            id = sprint.getId();
+        }
+        code = sprint.getCode();
+
+        ObjectType type = ObjectType.SPRINT;
+        Long parentId = null;
+
+        NodeTo nodeTo = new NodeTo( id, code, type, parentId );
+
+        nodeTo.setEnabled( sprint.isEnabled() );
+
+        return nodeTo;
+    }
+
+    @Override
+    public NodeTo fromTask(TaskTo task) {
+        if ( task == null ) {
+            return null;
+        }
+
+        long id = 0L;
+        String code = null;
+        Long parentId = null;
+
+        if ( task.getId() != null ) {
+            id = task.getId();
+        }
+        code = task.getCode();
+        parentId = task.getParentId();
+
+        ObjectType type = ObjectType.TASK;
+
+        NodeTo nodeTo = new NodeTo( id, code, type, parentId );
+
+        nodeTo.setEnabled( task.isEnabled() );
+
+        return nodeTo;
+    }
+}
